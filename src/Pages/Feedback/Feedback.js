839d1cc5669411ae963fcf75149fd7b49430d8ba
Andrii.Zinchenko <Fiaroa4701a>
2020-05-27 21:07:17 +0300

    review and improve

--- src/Pages/Feedback/Feedback.js
+++ src/Pages/Feedback/Feedback.js
@@ -3,35 +3,90 @@ import React, { useState } from 'react';
 import "./Feedback.css"
 const url = 'http://localhost:3001/feedbacks';
 
-const Feedback = (props) => {
+// example of component - put to diff folder in future.
+const FeedbackMessage = ({ message, onDelete }) => (
+  <div>
+    <div>{message.author}</div>
+    <div>{message.title}</div>
+    <button onClick={onDelete}>delete message</button>
+    <hr/>
+  </div>
+);
+
+const Feedback = () => {
     const [text, setText] = useState('');
     const [name, setName] = useState('');
-    const textChange =  e => {setText({text: e.target.value})};
-    const nameChange =  e => {setName({name: e.target.value})};
-    const myFetch = () => (fetch (url)
+    const [messages, setMessage] = useState([]);
+    
+    const textChange =  React.useCallback((e) => {
+      setText(e.target.value);
+    }, []);
+    
+    const nameChange =  React.useCallback((e) => {
+      setName(e.target.value);
+    }, []);
+    
+    const getMessage = React.useCallback(() => (
+      fetch (url)
         .then ((res) => res.json())
         .then((res) => {
-            console.log(res)
-        }))
-    const getMessage = () => {
-        myFetch()
-    };
-    const postMessage = () => {
-        fetch(url, {method: "POST", headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({text, name})
-        });
-        // setTitle({title:null})
-    }
-    console.log(text);
-    console.log(name);
+          setMessage([...res]);
+        })
+    ));
+    
+    React.useEffect(() => {
+      getMessage();
+    }, [])
+    
+    const postMessage = React.useCallback(() => {
+      fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ title: text, author:name })
+      }).then(() => {
+        getMessage();
+      });
+    }, [text, name])
+  
+    const deleteMessage = React.useCallback((id) => () => {
+      fetch(`${url}/${id}`, {
+        method: "DELETE",
+      }).then(() => {
+        getMessage();
+      });
+    }, [])
+
     return (
         <div className="Feedback">
             <h6>Type your text in form and press submit</h6>
-            <input placeholder="Enter your name" className="ModalAuthor" onChange={nameChange} value={toString().name}/>
-            <textarea cols="50" rows="3" placeholder="Type your message" className="ModalText"  onChange={textChange} value={toString().text}/>
-            <button onClick={getMessage}>GetMessage</button>
-            <button onClick={postMessage}>PostMessage</button>
-            <div className="FeedbackMessage" id={"M01"}>{}</div>
+            <input
+              placeholder="Enter your name"
+              className="ModalAuthor"
+              onChange={nameChange}
+              value={name}
+            />
+            <textarea
+              cols="50"
+              rows="3"
+              placeholder="Type your message"
+              className="ModalText"
+              onChange={textChange}
+              value={text}
+            />
+            <button
+              onClick={postMessage}
+            >
+              PostMessage
+            </button>
+            <div className="FeedbackMessage" id={"M01"}>
+              {messages.map((message) => (
+                <FeedbackMessage
+                  key={message?.id}
+                  onDelete={deleteMessage(message?.id)}
+                  message={message}
+                />
+              ))}
+            </div>
         </div>
         );
 };
