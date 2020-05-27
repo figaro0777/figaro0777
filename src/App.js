29639ec952b022c4d59226d05bec00c63b61ede8
figaro0777 <63141959+figaro0777@users.noreply.github.com>
2020-05-27 20:43:16 +0300

    'new1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 import Header from "./components/Header/Header";
-import {Products, Authorized, Homepage, Gallery, Feedback, About } from "./Pages/index";
+import {Products, Authorized, Homepage, Gallery, Feedback, About } from "./Pages";
 
 function App() {
   return (
